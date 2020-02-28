3af48ac92c1bdda9ea6a6b82b8b3ac1d94e44081
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-28 18:41:00 +0100

    Add two-fer

---
+++ two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let twoFer (name : string option) : string =
+    match name with
+    | None -> "One for you, one for me."
+    | Some(name) -> "One for " + name + ", one for me."
