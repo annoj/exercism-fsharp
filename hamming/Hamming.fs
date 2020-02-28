1e30e5a2fff48ebf68453ba0c1f7bec519552e50
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-28 18:41:29 +0100

    Add hamming

---
+++ hamming/Hamming.fs
@@ -0,0 +1,8 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option = 
+    if strand1.Length <> strand2.Length then None
+    else 
+        let dist = Array.zip (strand1.ToCharArray()) (strand2.ToCharArray())
+                |> Array.fold (fun acc (x, y) -> acc + (if x = y then 0 else 1)) 0
+        Some dist
