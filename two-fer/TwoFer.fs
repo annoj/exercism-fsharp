ef8cd1ed9a007aa7d4ac3642f1e50b98a3862453
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-28 20:19:21 +0100

    two-fer: Refactor to use Option.default

--- two-fer/TwoFer.fs
+++ two-fer/TwoFer.fs
@@ -1,6 +1,6 @@
 ﻿module TwoFer
 
 let twoFer (name : string option) : string =
-    match name with
-    | None -> "One for you, one for me."
-    | Some(name) -> "One for " + name + ", one for me."
+    name
+    |> Option.defaultValue "you"
+    |> sprintf "One for %s, one for me." 
