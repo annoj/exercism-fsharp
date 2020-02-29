99adeb4540b50380f02f908fe658012d25cd719d
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-29 17:47:46 +0100

    hamming: Rewrite using Seq.map2

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -2,7 +2,7 @@
 
 let distance (strand1: string) (strand2: string): int option = 
     if strand1.Length <> strand2.Length then None
-    else 
-        let dist = Array.zip (strand1.ToCharArray()) (strand2.ToCharArray())
-                |> Array.fold (fun acc (x, y) -> acc + (if x = y then 0 else 1)) 0
-        Some dist
+    else (strand1, strand2)
+        ||> Seq.map2 (=) 
+        |> Seq.sumBy (fun b -> if b then 0 else 1)
+        |> Some
