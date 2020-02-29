c2d4f4cfdb7e3d29d8cb23ffb20e410767769338
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-29 21:15:09 +0100

    Add isogram

---
+++ isogram/Isogram.fs
@@ -0,0 +1,6 @@
+﻿module Isogram
+
+let isIsogram (str: string) =
+    let onlyLetters = Seq.filter (fun (c: char) -> List.contains c ['a' .. 'z']) (str.ToLower())
+    let onlyLettersDistinct = onlyLetters |> Seq.distinct
+    (onlyLetters |> Seq.length) = (onlyLettersDistinct |> Seq.length)
