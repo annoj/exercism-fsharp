f0021c274cda0e4eedcea3a1512f387fc380d9dd
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-29 19:16:08 +0100

    Add pangram

---
+++ pangram/Pangram.fs
@@ -0,0 +1,11 @@
+﻿module Pangram
+
+let isPangram (input: string): bool = 
+    let isLetter (c: char) =
+        0x40 < (int c) && (int c) < 0x5b || // A - Z
+        0x60 < (int c) && (int c) < 0x7b    // a - z
+    
+    input
+    |> Seq.filter isLetter
+    |> Seq.distinctBy (fun c -> if int c > 0x5b then int c - 0x20 else int c) // to lowercase
+    |> Seq.length = 26
