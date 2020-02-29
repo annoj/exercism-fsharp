bfc6c106b1c3f11ecf1e079d32834ff347e33ca7
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-29 19:19:46 +0100

    pangram: Use SeanMartz's solution

--- pangram/Pangram.fs
+++ pangram/Pangram.fs
@@ -1,11 +1,7 @@
 ﻿module Pangram
 
 let isPangram (input: string): bool = 
-    let isLetter (c: char) =
-        0x40 < (int c) && (int c) < 0x5b || // A - Z
-        0x60 < (int c) && (int c) < 0x7b    // a - z
-    
-    input
-    |> Seq.filter isLetter
-    |> Seq.distinctBy (fun c -> if int c > 0x5b then int c - 0x20 else int c) // to lowercase
-    |> Seq.length = 26
+    let lower = input.ToLower()
+    ['a' .. 'z']
+    |> List.except lower
+    |> List.isEmpty
