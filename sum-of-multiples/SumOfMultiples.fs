33f538e789b5053012f11f9266872f91f94b56eb
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-29 22:38:33 +0100

    Add sum-of-multiples

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,10 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int =
+    let listOfMultiples (n: int) =
+        [for k in [0 .. (upperBound - 1) / n] do yield n * k]
+    numbers
+    |> List.filter (fun n -> n > 0)
+    |> List.collect listOfMultiples
+    |> List.distinct
+    |> List.sum
