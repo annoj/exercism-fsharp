539ab0f85eb77f6ce270e6c11ae688c716bb3aa3
Jona <s_heitzer@stud.hwr-berlin.de>
2020-02-29 21:26:04 +0100

    isogram: Make solution more idiomatic

--- isogram/Isogram.fs
+++ isogram/Isogram.fs
@@ -1,6 +1,10 @@
 ﻿module Isogram
 
 let isIsogram (str: string) =
-    let onlyLetters = Seq.filter (fun (c: char) -> List.contains c ['a' .. 'z']) (str.ToLower())
-    let onlyLettersDistinct = onlyLetters |> Seq.distinct
-    (onlyLetters |> Seq.length) = (onlyLettersDistinct |> Seq.length)
+    let letters =
+        str.ToLower()
+        |> Seq.filter System.Char.IsLetter
+    letters
+    |> Seq.distinct
+    |> Seq.length
+    |> (=) (Seq.length letters)
