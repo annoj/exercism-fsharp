3ff0ef283164f54e21e117a1f3f670d305b48072
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-01 15:07:25 +0100

    leap: Refactor if year % 400 = 0

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -3,10 +3,7 @@
 let leapYear (year: int): bool =
     if year % 4 = 0 then
         if year % 100 = 0 then 
-            if year % 400 = 0 then
-                true
-            else
-                false
+            year % 400 = 0
         else
             true
     else
