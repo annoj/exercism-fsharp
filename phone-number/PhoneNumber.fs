4887ebde72294ab6eb9974d20008f74f7405ab13
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-01 17:50:27 +0100

    phone-number: Use kirchnergo's solution

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -1,19 +1,44 @@
 ﻿module PhoneNumber
 
-let debug a = 
-    a |> Seq.iter (fun x -> printf "%s" (string x)); printfn ""
-    printfn "%s" (a.GetType().ToString())
-    a
+open System
 
-open System.Text.RegularExpressions
-let (|Regex|_|) (pattern:seq<char>) (input: seq<char>) =
-    let m = Regex.Match(input.ToString(), pattern.ToString())
-    if m.Success then Some (m.Groups.[0]) else None
+let clean input =
+    let notvalidPunct c =
+        (Char.IsPunctuation c) && not([ '.'; '('; ')'; '-' ] |> List.contains c)
 
-let clean input : uint64 option =
-    let onlyDigits = input |> Seq.filter System.Char.IsDigit
-    debug onlyDigits |> ignore
-    match onlyDigits with
-    | Regex @"^[2-9]\d{2}[2-9]\d{2}\d{4}$" number -> Some (uint64 (string onlyDigits))
-    | Regex @"^1[2-9]\d{2}[2-9]\d{2}\d{4}$" number -> Some (uint64 (string onlyDigits).[1..])
-    | _ -> None
+    let checkChars s =
+        match s with
+        | s when String.exists Char.IsLetter s -> Error "letters not permitted"
+        | s when String.exists notvalidPunct s -> Error "punctuations not permitted"
+        | _ -> Ok s
+
+    let checkLength (s: string) =
+        match s.Length with
+        | s when s > 11 -> Error "more than 11 digits"
+        | s when s < 10 -> Error "incorrect number of digits"
+        | _ -> Ok s
+
+    let checkCountryCode (s: string) =
+        match String.length s, s.[0] = '1' with
+        | (11, false) -> Error "11 digits must start with 1"
+        | (11, true) -> Ok s.[1..]
+        | _ -> Ok s
+
+    let checkDigitNotZeroOrOne n desc (s: string) =
+        match s.[n] with
+        | '0' -> Error(sprintf "%s cannot start with zero" desc)
+        | '1' -> Error(sprintf "%s cannot start with one" desc)
+        | _ -> Ok s
+
+    let checkAreaCode = checkDigitNotZeroOrOne 0 "area code"
+    
+    let checkExchangeCode = checkDigitNotZeroOrOne 3 "exchange code"
+
+    input
+    |> checkChars
+    |> Result.map (String.filter Char.IsNumber)
+    |> Result.bind checkLength
+    |> Result.bind checkCountryCode
+    |> Result.bind checkAreaCode
+    |> Result.bind checkExchangeCode
+    |> Result.map UInt64.Parse
