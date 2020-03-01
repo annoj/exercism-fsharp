3dee9f528ea86001eba4839f229bb5180d16ee00
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-01 15:14:08 +0100

    leap: Refactor to use only one statement

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,10 +1,5 @@
 ﻿module Leap
 
 let leapYear (year: int): bool =
-    if year % 4 = 0 then
-        if year % 100 = 0 then 
-            year % 400 = 0
-        else
-            true
-    else
-        false
+    year % 4 = 0 && year % 100 <> 0 ||
+    year % 4 = 0 && year % 100 = 0 && year % 400 = 0
