896a1790a24916002b5d5dfd52939fb0aaf257c1
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-01 17:34:11 +0100

    phone-number: WIP

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -10,10 +10,10 @@ let (|Regex|_|) (pattern:seq<char>) (input: seq<char>) =
     let m = Regex.Match(input.ToString(), pattern.ToString())
     if m.Success then Some (m.Groups.[0]) else None
 
-let clean input =
+let clean input : uint64 option =
     let onlyDigits = input |> Seq.filter System.Char.IsDigit
     debug onlyDigits |> ignore
     match onlyDigits with
-   // | Regex @"^[2-9]\d{2}[2-9]\d{2}\d{4}$" number -> Some (uint64 (string onlyDigits))
-   // | Regex @"^1[2-9]\d{2}[2-9]\d{2}\d{4}$" number -> Some (uint64 (string onlyDigits).[1..])
-    | _ -> Ok 2234567890UL
+    | Regex @"^[2-9]\d{2}[2-9]\d{2}\d{4}$" number -> Some (uint64 (string onlyDigits))
+    | Regex @"^1[2-9]\d{2}[2-9]\d{2}\d{4}$" number -> Some (uint64 (string onlyDigits).[1..])
+    | _ -> None
