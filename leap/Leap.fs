0589909f46a214386ba7706711af8867809693c0
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-04 17:33:29 +0100

    leap: Rearrange code to make boolean expression more clear

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -3,6 +3,5 @@
 let isDivisible dividend divisor = dividend % divisor = 0
 
 let leapYear (year: int): bool =
-    isDivisible year 4
-    && not (isDivisible year 100)
+    isDivisible year 4 && not (isDivisible year 100)
     || isDivisible year 400
