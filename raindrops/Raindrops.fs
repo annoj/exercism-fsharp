2e9379a457d82ad0a8465474d5cc875746ff5eb7
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-25 15:51:52 +0100

    Add raindrops

---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,11 @@
+﻿module Raindrops
+
+let convert (number: int): string =
+    let pling = if number % 3 = 0 then "Pling" else ""
+    let plang = if number % 5 = 0 then "Plang" else ""
+    let plong = if number % 7 = 0 then "Plong" else ""
+    let plingPlangPlong = pling + plang + plong
+  
+    match plingPlangPlong with
+    | "" -> number.ToString()
+    | s -> s
