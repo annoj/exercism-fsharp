5052423b2269ad82d24728d133cbf04fa1bbc205
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-25 16:26:13 +0100

    Add grains

---
+++ grains/Grains.fs
@@ -0,0 +1,9 @@
+﻿module Grains
+
+let square (n: int): Result<uint64,string> = 
+    match n with
+    | n when n > 0 && n < 65 -> Ok (1UL <<< (n - 1))
+    | _ -> Error "square must be between 1 and 64"
+
+let total: Result<uint64,string> =
+    Ok 18446744073709551615UL
