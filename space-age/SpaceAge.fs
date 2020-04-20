2c32d517a073eeaecd9d4aaff15c6c1dbc1706e0
Jona <s_heitzer@stud.hwr-berlin.de>
2020-04-20 20:51:25 +0200

    Add space-age

---
+++ space-age/SpaceAge.fs
@@ -0,0 +1,22 @@
+﻿module SpaceAge
+
+type Planet =
+    | Mercury
+    | Venus
+    | Earth
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+let age (planet: Planet) (seconds: int64): float =
+    match planet with
+    | Mercury -> (float) seconds / 7600543.81992
+    | Venus -> (float) seconds / 19414149.052176
+    | Earth -> (float) seconds / 31557600.0
+    | Mars -> (float) seconds / 59354032.690079994
+    | Jupiter -> (float) seconds / 374355659.124
+    | Saturn -> (float) seconds / 929292362.8848
+    | Uranus -> (float) seconds / 2651370019.3296
+    | Neptune -> (float) seconds / 5200418560.032001
